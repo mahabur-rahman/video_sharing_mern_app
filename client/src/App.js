0cedc14a3908a632cd618781f873509551f4b6c2
mahabur  rahman <annur4395@gmail.com>
2022-09-12 18:07:07 +0600

    sidebar ui added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,108 @@
-function App() {
+import styled from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Main = styled.div`
+  flex: 7;
+  background-color: #181818;
+`;
+
+const Wrapper = styled.div`
+  padding: 22px 96px;
+`;
+
+const App = () => {
   return (
-    <div>
-      Hello world!
-    </div>
+    <Container>
+      <Menu />
+      <Main>
+        <Wrapper>
+          <Navbar />
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+        </Wrapper>
+      </Main>
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/Menu.js
@@ -0,0 +1,176 @@
+import React from "react";
+import styled from "styled-components";
+import LogoImg from "../images/youtube.png";
+import {
+  Home,
+  ExploreOutlined,
+  AccountCircleOutlined,
+  LibraryMusicOutlined,
+  HistoryOutlined,
+  VideoLibraryOutlined,
+  SubscriptionsOutlined,
+  SportsBasketballOutlined,
+  SportsEsportsOutlined,
+  MovieOutlined,
+  ArticleOutlined,
+  LiveTvOutlined,
+  SettingsOutlined,
+  FlagOutlined,
+  HelpOutlineOutlined,
+  SettingsBrightnessOutlined,
+} from "@mui/icons-material";
+
+const Container = styled.div`
+  flex: 1;
+  background-color: black;
+  color: white;
+  height: 100vh;
+  font-size: 14px;
+  position: sticky;
+  top: 0;
+`;
+
+const Wrapper = styled.div`
+  padding: 18px 26px;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  font-weight: bold;
+  margin-bottom: 25px;
+`;
+const Img = styled.img`
+  height: 25px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  cursor: pointer;
+  padding: 7.5px 0;
+  transition: 0.2s all ease;
+
+  &:hover {
+    background-color: red;
+  }
+`;
+
+const Hr = styled.div`
+  margin: 15px 0px;
+  border: 0.5px solid #202020;
+`;
+
+const Login = styled.div``;
+
+const Button = styled.button`
+  padding: 5px 15px;
+  background-color: transparent;
+  border: 1px solid #3ea6ff;
+  color: #3ea6ff;
+  border-radius: 3px;
+  font-weight: 500;
+  margin-top: 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
+const Title = styled.h2`
+  font-size: 14px;
+  font-weight: 500;
+  color: #aaaaaa;
+  margin-bottom: 20px;
+`;
+
+const Menu = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Logo>
+          <Img src={LogoImg} />
+          YOUTUBE
+        </Logo>
+
+        <Item>
+          <Home />
+          Home
+        </Item>
+
+        <Item>
+          <ExploreOutlined />
+          Explore
+        </Item>
+        <Item>
+          <SubscriptionsOutlined />
+          Subscriptions
+        </Item>
+        <Hr />
+        <Item>
+          <VideoLibraryOutlined />
+          Library
+        </Item>
+        <Item>
+          <HistoryOutlined />
+          History
+        </Item>
+        <Hr />
+        <Login>
+          Sign in to like videos, comment, and subscribe.
+          <Button>
+            <AccountCircleOutlined />
+            SIGN IN
+          </Button>
+        </Login>
+        <Hr />
+        <Title>BEST OF LAMATUBE</Title>
+        <Item>
+          <LibraryMusicOutlined />
+          Music
+        </Item>
+        <Item>
+          <SportsBasketballOutlined />
+          Sports
+        </Item>
+        <Item>
+          <SportsEsportsOutlined />
+          Gaming
+        </Item>
+        <Item>
+          <MovieOutlined />
+          Movies
+        </Item>
+        <Item>
+          <ArticleOutlined />
+          News
+        </Item>
+        <Item>
+          <LiveTvOutlined />
+          Live
+        </Item>
+        <Hr />
+        <Item>
+          <SettingsOutlined />
+          Settings
+        </Item>
+        <Item>
+          <FlagOutlined />
+          Report
+        </Item>
+        <Item>
+          <HelpOutlineOutlined />
+          Help
+        </Item>
+        <Item>
+          <SettingsBrightnessOutlined />
+          Mode
+        </Item>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Menu;
