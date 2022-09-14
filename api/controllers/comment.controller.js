8cdc1a768bf46f635832ce95a5fea6e65baede3d
mahabur  rahman <annur4395@gmail.com>
2022-09-14 21:50:17 +0600

    delete comment logic added

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,4 +1,6 @@
+const createError = require("../error");
 const CommentModel = require("../models/Comment.model");
+const VideoModel = require("../models/Video.model");
 
 // create a comment
 const addComment = async (req, res, next) => {
@@ -12,5 +14,26 @@ const addComment = async (req, res, next) => {
   }
 };
 
+// DELETE COMMENT
+const deleteComment = async (req, res, next) => {
+  try {
+    const comment = await CommentModel.findById(req.params.id);
+    // console.log(comment);
+
+    const video = await VideoModel.findById(req.params.id);
+    console.log(video);
+
+    if (req.user.id === comment.userId || req.user.id === video.userId) {
+      await CommentModel.findByIdAndDelete(req.params.id);
+
+      return res.status(200).json("The comment has been deleted.");
+    } else {
+      return next(createError(403, "You can delete ony your comment!"));
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
 // exports
-module.exports = { addComment };
+module.exports = { addComment, deleteComment };
--- api/routes/comments.route.js
+++ api/routes/comments.route.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const verifyToken = require("../verifyToken");
-const { addComment } = require("../controllers/comment.controller");
+const {
+  addComment,
+  deleteComment,
+} = require("../controllers/comment.controller");
 
 // ADD COMMENT
 router.post("/", verifyToken, addComment);
+
+// DELETE COMMENT
+router.delete("/:id", verifyToken, deleteComment);
 
 // export
 module.exports = router;
