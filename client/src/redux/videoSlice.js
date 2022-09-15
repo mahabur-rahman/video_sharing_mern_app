a2f776a51ec6451c79115c19bc00287b466a15ea
mahabur  rahman <annur4395@gmail.com>
2022-09-15 16:23:32 +0600

    persist redux store

---
+++ client/src/redux/videoSlice.js
@@ -0,0 +1,43 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// initialState
+const initialState = {
+  currentUser: null,
+  loading: false,
+  error: false,
+};
+
+// create slice
+export const userSlice = createSlice({
+  name: "video",
+  initialState,
+
+  reducers: {
+    // login
+    loginStart: (state) => {
+      state.loading = true;
+    },
+
+    loginSuccess: (state, action) => {
+      state.loading = false;
+      state.currentUser = action.payload;
+    },
+
+    loginFailure: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+
+    // logout
+    logout: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = false;
+    },
+  },
+});
+
+export const { loginStart, loginSuccess, loginFailure, logout } =
+  userSlice.actions;
+
+export default userSlice.reducer;
